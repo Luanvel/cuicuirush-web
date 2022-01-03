22df0a8111eb297a2017eb6a19f47f8371ee44b0
Enric Mir <enricmir1993@gmail.com>
2022-01-03 20:15:28 +0100

    updated web

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,77 @@
 <script setup>
 import { ref } from 'vue'
 import bgStart from '../assets/bg_start.png'
+import adoptionGirl from '../assets/adoptiongirl.png'
+import marketGirl from '../assets/marketgirl.png'
 
 defineProps({
 })
 </script>
 
 <template>
-  <div class="bg-gradient-radial from-blue-400 to-blue-50 w-full h-full">
-    <div class="bg-start h-full bg-no-repeat bg-cover pt-10"></div>
+  <div class="bg-gradient-radial from-blue-400 to-blue-50">
+    <div class="bg-start bg-center bg-no-repeat">
+      <img :src="bgStart" class="invisible" />
+    </div>
   </div>
-  <div class="bg-grass p-3">
-    <p
-      class="py-5"
-    >CuicuiRush is an NFT game coming in 2022 where one can become a cuicui racing expert. You can breed, evolve, give them muuuch love and prepare them to race faster than a thunder!</p>
-    <p class="py-5">Become the greatest Cuicui Trainer of your generation in this exciting game</p>
-    <p class="py-5">And win some money while you have fun with it.</p>
+  <div class="bg-grass py-10">
+    <div class="flex flex-col lg:flex-row m-auto w-full lg:w-1/2 lg:rounded-2xl overflow-hidden">
+      <div class="lg:w-1/2">
+        <img :src="adoptionGirl" />
+      </div>
+      <div
+        class="bg-white opacity-50 text-center lg:w-1/2 flex flex-col justify-center pt-2 lg:pt-0"
+      >
+        <h1 class="font-bold text-md lg:text-5xl">Adopt</h1>
+        <p class="py-3">Adopt new cuicuis from our lovely Petkeeper</p>
+      </div>
+    </div>
   </div>
+  <div class="bg-grass py-10">
+    <div
+      class="flex flex-col-reverse lg:flex-row m-auto w-full lg:w-1/2 lg:rounded-2xl overflow-hidden"
+    >
+      <div
+        class="bg-white opacity-50 text-center lg:w-1/2 flex flex-col justify-center pt-2 lg:pt-0"
+      >
+        <h1 class="font-bold text-md lg:text-5xl">Evolve</h1>
+        <p
+          class="py-3"
+        >Feed your cuicuis with the right meal to evolve them to the next level. Discover new species as you play!</p>
+      </div>
+      <div class="lg:w-1/2">
+        <img :src="marketGirl" />
+      </div>
+    </div>
+  </div>
+  <div class="bg-grass py-2 lg:px-60 lg:py-16">
+    <div class="bg-white opacity-50 p-8 lg:rounded-2xl text-center">
+      <h1 class="font-bold text-md lg:text-5xl">Race</h1>
+      <p class="py-3">Equip and train your cuicuis so they never lose a race!</p>
+    </div>
+  </div>
+  <div class="bg-grass py-2 pt-10 lg:pt-0 lg:px-60 lg:py-16">
+    <div class="bg-white opacity-50 p-8 lg:rounded-2xl text-center">
+      <h1 class="font-bold text-md lg:text-5xl">Become the #1</h1>
+      <p
+        class="py-3"
+      >Keep training, keep feeding, keep bredding, keep RACING and become the numero uno.</p>
+    </div>
+  </div>
+  <footer class="bg-gray-700 py-2 pt-10 lg:p-16 lg:px-60">
+    <div class="opacity-80 lg:p-8 lg:rounded-2xl text-white text-center">
+      <p>CuicuiRush is an NFT game coming in 2022. You can breed, evolve, equip, train and give your cuicuis muuuch love (and other type of potions) so they can race faster than a THUNDER!</p>
+      <p>Follow our newsletter to be the first to know when we launch!</p>
+      <p>
+        Follow us on
+        <a
+          href="https://www.instagram.com/cuicuirush/"
+          class="font-bold"
+          target="_blank"
+        >INSTAGRAM</a>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -1,18 +1,22 @@
 <script setup>
 import { ref } from 'vue'
+import bgStart from '../assets/bg_start.png'
 
 defineProps({
 })
 </script>
 
 <template>
-  <div class="bg-gradient-radial from-blue-400 to-blue-50 w-full h-full">
-    <div class="bg-start bg-no-repeat bg-cover w-full h-full">
-      <iframe
-        width="100%"
-        height="100%"
-        src="https://cdn.forms-content.sg-form.com/8337b6b8-629c-11ec-a2e8-faafea8671e9"
-      />
+  <div class="bg-gradient-radial from-blue-400 to-blue-50">
+    <div class="bg-start bg-center bg-no-repeat relative">
+      <img :src="bgStart" class="invisible" />
+      <div class="absolute top-0 left-0 w-full h-full">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://cdn.forms-content.sg-form.com/8337b6b8-629c-11ec-a2e8-faafea8671e9"
+        />
+      </div>
     </div>
   </div>
 </template>
